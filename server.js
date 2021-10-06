ac41522664c8c0c46fc0f636def38167c83fa9a1
Jaursch <burton.jaursch@gmail.com>
2021-10-05 20:49:00 -0700

    adding info call

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { json } = require("express");
 const express = require("express");
 const path = require("path");
 const powerclip = require('./service.js');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || "8080";
@@ -14,7 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/validate", (req, res) => {
-	console.log("body: " + JSON.stringify(req.body));
+	console.log("/validate - get");
+	//onsole.log("body: " + JSON.stringify(req.body));
 	if(!Object.keys(req.body).length){
 		res.status(400).send("No body provided");
 	}else if(!req.body.url){
@@ -23,11 +25,43 @@ app.get("/validate", (req, res) => {
 		const url = req.body.url;
 		const valid = powerclip.validate(url);
 
-		let msg = valid? `'${url}' is a valid YouTube link!`: `'${url}' is not a valid YouTube link`;
+		let msg = valid? `'${url}' is a valid YouTube link!`: `'${url}' is not a valid YouTube url`;
 
 		res.status(200).send(msg);
 	}
-	
+});
+
+app.get("/info", async (req, res) => {
+	console.log("/info - get");
+
+	if(!Object.keys(req.body).length){
+		res.status(400).send("No body provided");
+	}else if(!req.body.url){
+		res.status(400).send("url needs to be in json format w/ key \"url\"");
+	}else{
+		const url = req.body.url;
+
+		const valid = powerclip.validate(url);
+		if(!valid){
+			res.status(400).send(`'${url}' is not a valid YouTube url`);
+		}else{
+			//get filepath of information
+			let infopath = await powerclip.info(url);
+			console.log("infopath: " + infopath);
+
+			let data = fs.readFileSync(infopath);
+			fs.readFile(infopath, (err, data)=>{
+				if(err){
+					console.err("err: " + err);
+					res.status(500).send("error parsing file: " + err);
+				}else{					
+					let info = JSON.parse(data);
+					res.status(200).send(info.videoDetails);
+				}
+			})
+		}
+	}
+
 });
 
 app.listen(port, () => {
