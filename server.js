26d13b9fc421d95ecc65325f937e7e73bb94c89f
Jaursch <burton.jaursch@gmail.com>
2021-10-05 19:30:35 -0700

    validate path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const express = require("express");
 const path = require("path");
 const powerclip = require('./service.js');
@@ -5,9 +6,28 @@ const powerclip = require('./service.js');
 const app = express();
 const port = process.env.PORT || "8080";
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	console.log(`Getting a basic request`);
 	res.status(200).send("Basic request for Powerclip!");
+});
+
+app.get("/validate", (req, res) => {
+	console.log("body: " + JSON.stringify(req.body));
+	if(!Object.keys(req.body).length){
+		res.status(400).send("No body provided");
+	}else if(!req.body.url){
+		res.status(400).send("url needs to be in json format w/ key \"url\"");
+	}else{
+		const url = req.body.url;
+		const valid = powerclip.validate(url);
+
+		let msg = valid? `'${url}' is a valid YouTube link!`: `'${url}' is not a valid YouTube link`;
+
+		res.status(200).send(msg);
+	}
+	
 });
 
 app.listen(port, () => {
